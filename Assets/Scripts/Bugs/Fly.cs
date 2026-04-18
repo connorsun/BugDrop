03ded1cfa5492499c14c7b618e0593d0a903176c
connorsun <connorsun@gmail.com>
2026-04-18 16:51:58 -0700

    Vertical Bug Height

--- Assets/Scripts/Bugs/Fly.cs
+++ Assets/Scripts/Bugs/Fly.cs
@@ -11,7 +11,7 @@ public class Fly : Bug
     // Gets metadata about this bug type
     public static BugInfo GetInfo()
     {
-        return new BugInfo("Fly", 1, 1, 1.5f);
+        return new BugInfo("Fly", 1, 1, 1.5f, 0.5f);
     }
 
     // --- PUBLIC METHODS ---
