5ed5efb1584b4bb3a09a73c9077b2cc69515b4de
connorsun <connorsun@gmail.com>
2026-04-19 18:40:56 -0700

    attempt at tooltip bounds

--- Assets/Scripts/Tooltip.cs
+++ Assets/Scripts/Tooltip.cs
@@ -6,10 +6,13 @@ public class Tooltip : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI tooltipText;
     private const float DETECTION_RADIUS = 0.05f;
+    private const float X_MARGIN = 16;
+    private const float Y_MARGIN = 16;
+    private Vector4 baseMargins;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        baseMargins = tooltipText.margin;
     }
 
     // Update is called once per frame
@@ -25,9 +28,25 @@ public class Tooltip : MonoBehaviour
             {
                 tooltipText.text = "[" + bug.baseScore + "] " + bug.thisBugInfo.tooltip;
                 transform.position = mouseWorldPos;
+                UpdateBounds();
                 return;
             }
         }
         tooltipText.text = "";
+        UpdateBounds();
+    }
+    private void UpdateBounds()
+    {
+        tooltipText.ForceMeshUpdate();
+        Bounds textBounds = tooltipText.textBounds;
+        print(textBounds);
+        if (textBounds.min.y < Y_MARGIN)
+        {
+            tooltipText.margin = baseMargins + new Vector4(0f, textBounds.min.y - Y_MARGIN, 0f, 0f);
+        } else
+        {
+            tooltipText.margin = baseMargins;
+        }
+        
     }
 }
