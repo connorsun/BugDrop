40d2a79bd688fc369ab1c5c23b735a9b2b58cdeb
connorsun <connorsun@gmail.com>
2026-04-21 20:04:34 -0700

    termite

---
+++ Assets/Scripts/Bugs/Termite.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Threading.Tasks;
+
+
+public class Termite : Bug
+{
+    // --- CONSTANTS ---
+    // --- OBJECT REFERENCES --- 
+
+    // --- STATIC METADATA ---
+    // Gets metadata about this bug type
+    public static BugInfo GetInfo()
+    {
+        return new BugInfo("Termite", 2, 1, 1.5f, 0.5f, "+1 additional points for each Termite in tank, up to +7");
+    }
+
+    // --- PUBLIC METHODS ---
+    public override void Start()
+    {
+        this.thisBugInfo = GetInfo();
+        base.Start();
+    }
+
+    public override int CalculateOverallScore()
+    {
+        Termite[] termites = FindObjectsByType<Termite>();
+        return this.baseScore + (termites.Length - 1);
+    }
+
+    protected override async Task Score(bool isPrimary, int recursiveSecondaries)
+    {
+        ScorePoints(CalculateOverallScore(), isPrimary);
+    }
+}
