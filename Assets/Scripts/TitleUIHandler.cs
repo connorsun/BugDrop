051645f1e694031d44ac7fca99d56966dcdce3d4
connorsun <connorsun@gmail.com>
2026-04-23 20:11:19 -0700

    title screen go to arena

---
+++ Assets/Scripts/TitleUIHandler.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Collections;
+using System;
+using TMPro;
+using System.Linq;
+
+// Provides methods for rendering title UI
+public class TitleUIHandler : MonoBehaviour
+{
+    public void OnStartButtonClicked()
+    {
+        SceneManager.LoadScene("Arena");
+    }
+}
