086473f0eae153f99eda9d58b5adb47171d4d486
connorsun <connorsun@gmail.com>
2026-04-23 23:18:32 -0700

    Moving UI

--- Assets/Scripts/Tooltip.cs
+++ Assets/Scripts/Tooltip.cs
@@ -28,52 +28,54 @@ public class Tooltip : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        List<Collider2D> overlapColliders = new List<Collider2D>();
-        Vector3 mouseWorldPos = GameHandler.GetMouseWorldPos();
-        Physics2D.OverlapCircle(mouseWorldPos, GameHandler.MOUSE_DETECTION_RADIUS, ContactFilter2D.noFilter, overlapColliders);
-        foreach (Collider2D col in overlapColliders)
-        {
-            Bug bug = col.gameObject?.GetComponentInParent<Bug>();
-            if (bug != null && bug.IsStationary())
+        if (GameHandler.MovingBug == null) {
+            List<Collider2D> overlapColliders = new List<Collider2D>();
+            Vector3 mouseWorldPos = GameHandler.GetMouseWorldPos();
+            Physics2D.OverlapCircle(mouseWorldPos, GameHandler.MOUSE_DETECTION_RADIUS, ContactFilter2D.noFilter, overlapColliders);
+            foreach (Collider2D col in overlapColliders)
             {
-                if (prevBug != bug)
+                Bug bug = col.gameObject?.GetComponentInParent<Bug>();
+                if (bug != null && bug.IsStationary())
                 {
-                    if (prevBug != null) _ = prevBug.Hover(false, 0f, true);
-                    prevBug = bug;
-                    _ = bug.Hover(true, 0.3f, true);
+                    if (prevBug != bug)
+                    {
+                        if (prevBug != null) _ = prevBug.Hover(false, 0f, true);
+                        prevBug = bug;
+                        _ = bug.Hover(true, 0.3f, true);
+                    }
+
+                    cg.alpha = 1;
+                    titleText.text = bug.thisBugInfo.name;
+                    tooltipText.text = "[Base " + bug.baseScore + (bug.baseScore == 1 ? " point] " : " points] ") + bug.thisBugInfo.tooltip;
+                    tooltipText.ForceMeshUpdate();
+                    int lineCount = tooltipText.textInfo.lineCount;
+                    rt.sizeDelta = new Vector2(rt.sizeDelta.x, 25 + (lineCount * 8));
+
+                    float scale = 0.0625f;
+                    float w = (((rt.sizeDelta.x + bug.thisBugInfo.safeHorizRadius) / 2) + 8) * scale;
+                    float h = (((rt.sizeDelta.y + bug.thisBugInfo.safeVertRadius) / 2) + 8) * scale;
+
+                    float xOffset = ((bug.center.position.x + w) > (flipMarginX * scale)) ? -w : w;
+                    float yOffset = ((bug.center.position.y + h) > (flipMarginY * scale)) ? -h : h;
+
+                    transform.position = new Vector3(
+                        bug.center.position.x + xOffset,   // shift right by tooltip width
+                        bug.center.position.y + yOffset,   // shift up by tooltip height
+                        bug.center.position.z
+                    );
+
+                    return;
                 }
-
-                cg.alpha = 1;
-                titleText.text = bug.thisBugInfo.name;
-                tooltipText.text = "[Base " + bug.baseScore + (bug.baseScore == 1 ? " point] " : " points] ") + bug.thisBugInfo.tooltip;
-                tooltipText.ForceMeshUpdate();
-                int lineCount = tooltipText.textInfo.lineCount;
-                rt.sizeDelta = new Vector2(rt.sizeDelta.x, 25 + (lineCount * 8));
-
-                float scale = 0.0625f;
-                float w = (((rt.sizeDelta.x + bug.thisBugInfo.safeHorizRadius) / 2) + 8) * scale;
-                float h = (((rt.sizeDelta.y + bug.thisBugInfo.safeVertRadius) / 2) + 8) * scale;
-
-                float xOffset = ((bug.center.position.x + w) > (flipMarginX * scale)) ? -w : w;
-                float yOffset = ((bug.center.position.y + h) > (flipMarginY * scale)) ? -h : h;
-
-                transform.position = new Vector3(
-                    bug.center.position.x + xOffset,   // shift right by tooltip width
-                    bug.center.position.y + yOffset,   // shift up by tooltip height
-                    bug.center.position.z
-                );
-
-                return;
             }
-        }
 
-        if (prevBug != null)
-        {
-            _ = prevBug.Hover(false, 0f, true);
-            prevBug = null;
-        }
+            if (prevBug != null)
+            {
+                _ = prevBug.Hover(false, 0f, true);
+                prevBug = null;
+            }
 
-        cg.alpha = 0;
+            cg.alpha = 0;
+        }
         //UpdateBounds();
     }
     // private void UpdateBounds()
